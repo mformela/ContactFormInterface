dabf40ba029da6ea2dab9961a54c64abf2ae8702
Marta Formela <formela.marta@gmail.com>
2017-11-25 14:31:55 +0100

    a

--- AuthenticationCarUser/App_Start/IdentityConfig.cs
+++ AuthenticationCarUser/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace AuthenticationCarUser
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 4,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
